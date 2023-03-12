8b974133c41fd0ae404b42053833416bca7b0c7e
Aranyak Panigrahi <Aranyak Panigrahi>
2023-03-12 21:16:31 +0530

    added messages CRUD operations

--- src/base/views.py
+++ src/base/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 # Create your views here.
 
@@ -84,10 +84,22 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
     # for el in rooms:
     #     if el['id'] == int(pk):
     #         room = el
-    context = {'room': room}
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room',pk = room.id)
+    context = {'room': room, 'room_messages': room_messages,
+               'participants': participants}
 
     return render(request, 'room.html', context)
 
@@ -130,10 +142,24 @@ def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
 
     if request.user != room.host:
-        return HttpResponse('Only room admins can perforn this user')
+        return HttpResponse('Only room admins can perforn this operation')
 
     if request.method == 'POST':
         room.delete()
         return redirect('home')
 
     return render(request, 'delete.html', {'obj': room})
+
+
+@login_required(login_url='/login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('Only message admins can perforn this operation')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+
+    return render(request, 'delete.html', {'obj': message})

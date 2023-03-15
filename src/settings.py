06b34a871aead9172aac2052532588495eb5482e
Aranyak Panigrahi <Aranyak Panigrahi>
2023-03-15 12:52:57 +0530

    added railway to allowed hosts

--- src/settings.py
+++ src/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-5leo5he29%f0^!r@-jtaphqlr$(s2ghqeyw-#vw35@az_%bly$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['study-buddy-3k84.onrender.com',
-                 'study-buddy-production-4e42.up.railway.app/']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
